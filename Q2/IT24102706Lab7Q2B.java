9c81529c3234e95fef0479e22f515b338061090c
IT24102706 <it24102706@my.sliit.lk>
2024-10-03 23:36:50 +0530

    Add files via upload
    IT24102706Lab7Q2A.java , IT24102706Lab7Q2B.java , IT24102706Lab7Q2C.java files

---
+++ Q2/IT24102706Lab7Q2B.java
@@ -0,0 +1,16 @@
+	public class IT24102706Lab7Q2B {
+		public static void main(String[] args) { 
+				
+		int i , j ;
+		
+		for ( i =1; i<=5; i = i+1)
+		{
+			System.out.print( i + "-");
+			for (j=1; j<=i; j=j+1)
+		{
+			System.out.print("*");
+		}
+			System.out.println();
+			}
+	}
+}
